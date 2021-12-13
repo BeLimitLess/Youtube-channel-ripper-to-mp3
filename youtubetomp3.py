bd8a951a1bdff678c20d4d198a739e3e4c88c9fd
Mohit Kumar Saini <74623726+BeLimitLess@users.noreply.github.com>
2021-12-13 21:10:18 +0530

    Create youtubetomp3.py

---
+++ youtubetomp3.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+import os
+file1 = open('myfile.txt', 'r')
+Lines = file1.readlines()
+for line in Lines:
+    yt = YouTube(str(line.strip()))
+    # extract only audio
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download(output_path='.')
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    print(yt.title + " has been successfully downloaded.")
